8ba1a6c36e59a1aff1200fb32e2249bcfa1d6a9b
doyler <dev@doyler.net>
2013-03-29 01:04:42 -0500

    My employer lets me use work time to contribute to open source.The dog ate my commit.

--- generate_self_worth.py
+++ generate_self_worth.py
@@ -4,40 +4,50 @@ import random
 import subprocess
 
 messages = [
-  "My kid's appointment at the pediatrician can wait, I need to get this commit in.",
+  ("My kid's appointment at the pediatrician can wait, I need to get this"
+   " commit in."),
   "Sorry honey, I need to keep my streak going - we'll have sex tomorrow!",
   "This commit brought to you by my privilege.",
   "My employer lets me use work time to contribute to open source."
   "The dog ate my commit."
 ]
 
-rr = "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you"
+rr = ("Never gonna give you up Never gonna let you down Never gonna run around"
+      " and desert you Never gonna make you cry Never gonna say goodbye Never"
+      " gonna tell a lie and hurt you")
 
-if not subprocess.check_output('git rev-parse --abbrev-ref HEAD', stderr=subprocess.STDOUT).strip() == 'master':
+if not subprocess.check_output('git rev-parse --abbrev-ref HEAD',
+                               stderr=subprocess.STDOUT).strip() == 'master':
     raise Exception('Please run this from the master branch.')
 
-print 'Traveling back in time to the founding of Github! (actually, just a year.)'
+print ("Traveling back in time to the founding of Github! (actually, just"
+       " a year.)")
 start_date = datetime.datetime.now() - datetime.timedelta(days=365)
-end_date = datetime.datetime.now() + datetime.timedelta(days=1)
+#end_date = datetime.datetime.now() + datetime.timedelta(days=1)
+end_date = datetime.datetime.now() - datetime.timedelta(days=360)
 
 def daterange():
     for n in range(int ((end_date - start_date).days)):
         yield start_date + datetime.timedelta(n)
 
 for i, single_date in enumerate(daterange()):
-    str = rr.split(' ')[i % 34]
-    extra = "\n" if i % 34 == 0 else ' '
+    str = rr.split(" ")[i % 34]
+    extra = "\n" if i % 34 == 0 else " "
     if i != 0:
         str = extra + str
 
-    with open('self_worth.txt', 'a') as f:
+    with open("self_worth.txt", "a") as f:
         f.write(str)    
 
-    t = (single_date - datetime.datetime.utcfromtimestamp(0)).total_seconds() + 14400 + random.randint(0,57600)
+    t = ((single_date - datetime.datetime.utcfromtimestamp(0)).total_seconds()
+         + 14400 + random.randint(0,57600))
     os.putenv("GIT_COMMITTER_DATE", "{:.2f}".format(float(t)))
     os.putenv("GIT_AUTHOR_DATE", "{:.2f}".format(float(t)))    
-    os.popen('git add .').read()
-    subprocess.call('git commit -q -m \"' + random.choice(messages) + '\"', stderr=subprocess.STDOUT)
-    print "\r\nProving your worth with commits in " + single_date.strftime("%B, %Y") + "..."
+    subprocess.call("git add .", stderr=subprocess.STDOUT)
+    subprocess.call("git commit -q -m \"" + random.choice(messages) + "\"",
+                    stderr=subprocess.STDOUT)
+    print ("\r\nProving your worth with commit on " +
+           single_date.strftime("%B %d, %Y") + "...")
 
-print "\n\nNow show the world you are worth the physical space your body occupies - PUSH TO GITHUB! (`git push origin master`)"
+print ("\r\nNow show the world you are worth the physical space your body"
+       " occupies - PUSH TO GITHUB! (`git push origin master`)")
